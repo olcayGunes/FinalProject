b0ab46f495ae01776d4fa7b65cadcda89ed48b14
olcayGunes <65913895+olcayGunes@users.noreply.github.com>
2021-03-10 22:32:14 +0300

    13. Gün Ders Sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constants
 		public static string ProductNameInvalid = "Ürün İsmi Geçersiz";
 		public static string MaintenanceTime = "Sistem Bakımda";
 		public static string ProductListed = "Ürünler Listelendi";
+		public static string ProductCountOfCategoryError="Bir Kategorideki Maksimum Ürün Miktarına Erişildi";
+		public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var";
+		public static string CategoryLimitExceded="Kategori Limiti Aşıldı";
 	}
 }
